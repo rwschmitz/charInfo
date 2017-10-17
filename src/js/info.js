60cfc01b5cc5ebe45f0998999f1c48a36e2f7763
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-16 20:13:55 -0500

    updated for all classes

--- src/js/info.js
+++ src/js/info.js
@@ -1,8 +1,32 @@
-/* can use jquery get json */
 const endpoint = 'https://us.api.battle.net/wow/guild/Mal\'Ganis/step%20dad?fields=members&locale=en_US&apikey=c6ksxd9aqueqjtt3m4kxfkgett9k44m2';
-var btn = document.getElementById("btn");
-var list = document.getElementById("updateContent");
-let info = [];
+
+var btnDeathknight = document.getElementById("btnDeathknight");
+var btnDemonhunter = document.getElementById("btnDemonhunter");
+var btnDruid = document.getElementById("btnDruid");
+var btnHunter = document.getElementById("btnHunter");
+var btnMage = document.getElementById("btnMage");
+var btnMonk = document.getElementById("btnMonk");
+var btnPaladin = document.getElementById("btnPaladin");
+var btnPriest = document.getElementById("btnPriest");
+var btnRogue = document.getElementById("btnRogue");
+var btnShaman = document.getElementById("btnShaman");
+var btnWarlock = document.getElementById("btnWarlock");
+var btnWarrior = document.getElementById("btnWarrior");
+
+var listDeathknight = document.getElementById("updateDeathknight");
+var listDemonhunter = document.getElementById("updateDemonhunter");
+var listDruid = document.getElementById("updateDruid");
+var listHunter = document.getElementById("updateHunter");
+var listMage = document.getElementById("updateMage");
+var listMonk = document.getElementById("updateMonk");
+var listPaladin = document.getElementById("updatePaladin");
+var listPriest = document.getElementById("updatePriest");
+var listRogue = document.getElementById("updateRogue");
+var listShaman = document.getElementById("updateShaman");
+var listWarlock = document.getElementById("updateWarlock");
+var listWarrior = document.getElementById("updateWarrior");
+
+var info = [];
 
 
 fetch(endpoint).then(function(blob) {       // first promise that is returned from the .then method, the data has no idea what kind of data it is.
@@ -12,19 +36,152 @@ fetch(endpoint).then(function(blob) {       // first promise that is returned fr
     });
 });
 
-var names = [];
+var deathknights = [];
+var demonhunters = [];
+var druids = [];
+var hunters = [];
+var mages = [];
+var monks = [];
+var paladins = [];
+var priests = [];
+var rogues = [];
+var shamans = [];
+var warlocks = [];
+var warriors = [];
 
+// check to see if array is empty
 
-function test () {
+function deathknight () {
     info.filter(function(char) {
-        if(char.character.class == 4) {
-            names.push(char.character.name + "<br>");
-            list.innerHTML = names.join('');
-            list.style.color = "yellow";
+        if(char.character.class == 6) { // deathknights
+            deathknights.push(char.character.name + "<br>");
+            listDeathknight.innerHTML = deathknights.join('');
+            listDeathknight.style.color = '#c41f3b';
+        }
+    });
+}
+
+function demonhunter () {
+    info.filter(function(char) {
+        if(char.character.class == 12) { // demonhunters
+            demonhunters.push(char.character.name + "<br>");
+            listDemonhunter.innerHTML = demonhunters.join('');
+            listDemonhunter.style.color = '#a330c9';
+        }
+    });
+}
+
+function druid () {
+    info.filter(function(char) {
+        if(char.character.class == 11) { // druids
+            druids.push(char.character.name + "<br>");
+            listDruid.innerHTML = druids.join('');
+            listDruid.style.color = '#ff7d0a';
+        }
+    });
+}
+
+function hunter () {
+    info.filter(function(char) {
+        if(char.character.class == 3) { // hunters
+            hunters.push(char.character.name + "<br>");
+            listHunter.innerHTML = hunters.join('');
+            listHunter.style.color = '#abd473';
+        }
+    });
+}
+
+function mage () {
+    info.filter(function(char) {
+        if(char.character.class == 8) { // mages
+            mages.push(char.character.name + "<br>");
+            listMage.innerHTML = mages.join('');
+            listMage.style.color = '#69ccf0';
+        }
+    });
+}
+
+function monk () {
+    info.filter(function(char) {
+        if(char.character.class == 10) { // monks
+            monks.push(char.character.name + "<br>");
+            listMonk.innerHTML = monks.join('');
+            listMonk.style.color = '#00ff96';
+        }
+    });
+}
+
+function paladin () {
+    info.filter(function(char) {
+        if(char.character.class == 2) { // paladins
+            paladins.push(char.character.name + "<br>");
+            listPaladin.innerHTML = paladins.join('');
+            listPaladin.style.color = '#f58cba';
+        }
+    });
+}
+
+function priest () {
+    info.filter(function(char) {
+        if(char.character.class == 5) { // priests
+            priests.push(char.character.name + "<br>");
+            listPriest.innerHTML = priests.join('');
+            listPriest.style.color = '#ffffff';
+        }
+    });
+}
+
+function rogue () {
+    info.filter(function(char) {
+        if(char.character.class == 4) { // rogues
+            rogues.push(char.character.name + "<br>");
+            listRogue.innerHTML = rogues.join('');
+            listRogue.style.color = '#fff569';
+        }
+    });
+}
+
+function shaman () {
+    info.filter(function(char) {
+        if(char.character.class == 7) { // shamans
+            shamans.push(char.character.name + "<br>");
+            listShaman.innerHTML = shamans.join('');
+            listShaman.style.color = '#0070d3';
+        }
+    });
+}
+
+function warlock () {
+    info.filter(function(char) {
+        if(char.character.class == 9) { // warlocks
+            warlocks.push(char.character.name + "<br>");
+            listWarlock.innerHTML = warlocks.join('');
+            listWarlock.style.color = '#9482c9';
+        }
+    });
+}
+
+function warrior () {
+    info.filter(function(char) {
+        if(char.character.class == 1) { // warriors
+            warriors.push(char.character.name + "<br>");
+            listWarrior.innerHTML = warriors.join('');
+            listWarrior.style.color = '#c79c63';
         }
     });
 }
 
 
-btn.addEventListener("click", test, false);
+btnDeathknight.addEventListener("click", deathknight, false);
+btnDemonhunter.addEventListener("click", demonhunter, false);
+btnDruid.addEventListener("click", druid, false);
+btnHunter.addEventListener("click", hunter, false);
+btnMage.addEventListener("click", mage, false);
+btnMonk.addEventListener("click", monk, false);
+btnPaladin.addEventListener("click", paladin, false);
+btnPriest.addEventListener("click", priest, false);
+btnRogue.addEventListener("click", rogue, false);
+btnShaman.addEventListener("click", shaman, false);
+btnWarlock.addEventListener("click", warlock, false);
+btnWarrior.addEventListener("click", warrior, false);
 
